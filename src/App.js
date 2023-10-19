5ef139a66760364516b4d02d1dbfe5718a4689e4
Steven Zavala <55272248+steven3542@users.noreply.github.com>
2023-10-19 03:23:21 -0400

    latest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import logo from "./logo.svg";
+import { image45 } from "./images/real-back";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="clasuda">
-      <h1>hi</h1>
-    </div>
-  );
+  return <div className="clasuda" />;
 }
 
 export default App;
