a08999f9b7b3b7bc7d3d0cc0480405e483fd775e
Steven Zavala <55272248+steven3542@users.noreply.github.com>
2023-10-19 03:54:17 -0400

    background image perfect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
-import { image45 } from "./images/real-back";
+import image45 from "./images/real-back.png";
 import "./App.css";
 
 function App() {
-  return <div className="clasuda" />;
+  return <div></div>;
 }
 
 export default App;
