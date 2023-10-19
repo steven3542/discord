7058d02bc490a3e26178c9a7b9453fcb313624be
Steven Zavala <55272248+steven3542@users.noreply.github.com>
2023-10-19 05:09:39 -0400

    final draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import image45 from "./images/poke-logo.png";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="image-decor">
       <img src={image45} />
       <h1>@pokemoncardsripstcglive</h1>
+      <div className="text-bottom">Rip N Ship Live Everyday!</div>
+
+      <div className="mb-2">
+        <Button
+          variant="primary"
+          size="lg"
+          href="https://discord.gg/cZA6MPVKYx"
+          target="_blank"
+        >
+          DISCORD
+        </Button>
+      </div>
     </div>
   );
 }
