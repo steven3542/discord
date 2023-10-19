155d76c3a9bdaad275442150b994e29d37307397
Steven Zavala <55272248+steven3542@users.noreply.github.com>
2023-10-19 13:44:06 -0400

    its responsive now

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import image45 from "./images/poke-logo.png";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function App() {
   return (
-    <div className="image-decor">
-      <img src={image45} />
-      <h1>@pokemoncardsripstcglive</h1>
-      <div className="text-bottom">Rip N Ship Live Everyday!</div>
+    <Container className="image-decor">
+      <Row>
+        <Col>
+          <img src={image45} />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <h1>@pokemoncardsripstcglive</h1>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <h1 className="text-bottom">Rip N Ship Live Everyday!</h1>
+        </Col>
+      </Row>
 
       <div className="mb-2">
         <Button
@@ -21,7 +38,7 @@ function App() {
           DISCORD
         </Button>
       </div>
-    </div>
+    </Container>
   );
 }
 
